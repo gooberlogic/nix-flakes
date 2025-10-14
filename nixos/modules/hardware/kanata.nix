043826222fcff89c9e1030a165502a4b46b88583
gooberlogic <gooberlogic@proton.me>
2025-10-14 14:02:37 -0400

    add navigation layer

--- nixos/modules/hardware/kanata.nix
+++ nixos/modules/hardware/kanata.nix
@@ -15,14 +15,14 @@
       )
 
       ;; Gallium v2 + classic angle mod
-      ;; (Bryson's angle mod has too many key changes)
+      ;; (Bryson's angle mod has too many changes)
       (deflayer gallium
         esc  f1   f2   f3   f4   f5   f6   f7   f8   f9   f10  f11  f12 
         @grl 1    2    3    4    5    6    7    8    9    0    -    =    bspc
         tab  b    l    d    c    v    j    f    o    u    ,    [    ]    \
         @cap n    r    t    s    g    y    h    a    e    i    /    ret
         lsft q    m    w    z    x    k    p    '    ;    .    rsft
-        lctl lmet lalt           spc            ralt rmet rctl
+        @lct lmet lalt           spc            ralt rmet @rct
       )
 
       ;; Colemak-DH (angle modded)
@@ -51,7 +51,10 @@
         cmk (layer-switch colemakdh)
         qwr (layer-switch qwerty)
       
-        cap (tap-hold 200 200 caps lctl)
+        cap (tap-hold 200 200 caps (layer-toggle nav))
+
+        lct (multi lctrl (layer-while-held qwerty))
+        rct (multi rctrl (layer-while-held qwerty))
       )
       
       ;;(deflayermap (base)
@@ -64,6 +67,15 @@
         _    @gal @qwr @cmk _    _    _    _    _    _    _    lrld _    _
         _    _    _    _    _    _    _    _    _    _    _    _    _    _
         _    _    _    _    _    _    _    _    _    _    _    _    _
+        _    _    _    _    _    _    _    _    _    _    _    _
+        _    _    _              _              _    _    _
+      )
+
+      (deflayer nav 
+        _    _    _    _    _    _    _    _    _    _    _    _    _
+        _    _    _    _    _    _    _    _    _    _    _    _    _    _
+        _    _    _    _    _    _    _    _    _    _    _    _    _    _
+        _    _    _    _    _    _    lft  down up   rght esc  _    _
         _    _    _    _    _    _    _    _    _    _    _    _
         _    _    _              _              _    _    _
       )
