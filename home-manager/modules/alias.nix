19c5e0d6b02218c2a71b09318de781dbb30aec2e
gooberlogic <gooberlogic@proton.me>
2026-01-11 11:41:20 -0500

    flake update

--- home-manager/modules/alias.nix
+++ home-manager/modules/alias.nix
@@ -49,6 +49,7 @@ in
     density = "find -type f -exec dirname {} \\; | sort | uniq -c | sort -n";
     venv = "source ./.venv/bin/activate";
     pass-here = "export PASSWORD_STORE_DIR=$PWD";
+    wine-here = "export WINEPREFIX=$PWD";
     yt-dlp = "yt-dlp -S 'vcodec:avc,res,acodec:aac'";
     yt-dlp-archive = "yt-dlp -f '[height<=1080][fps<=30]' -S 'vcodec:avc,res,acodec:acc' -o '%(upload_date)s - %(title)s.%(ext)s' --download-archive ./archive.txt --write-description --write-info-json --write-sub --write-auto-sub --write-thumbnail";
     sensors-watch = "bash -c 'while true; do clear; sensors; sleep \$0; done'";
